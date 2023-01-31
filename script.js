30e75715cbe6d48dcf5764d5ab1de1642731aad1
Mikael Krefors <krefors@hotmail.com>
2023-01-31 11:12:01 +0100

    Done with a few more steps

--- script.js
+++ script.js
@@ -1,8 +1,55 @@
-// const BASE_URL = "https://jsonplaceholder.typicode.com/todos";
+const BASE_URL = "https://jsonplaceholder.typicode.com/todos/";
 const form = document.querySelector('.card form');
-
-// const output = document.querySelector('#output');
+// Lista för att spara todos
+const todos = []
+const todoList = document.querySelector('#output');
 // output.innerHTML = '';
+
+// GET function from the database//////////////////////
+const getTodos = () => {
+    // fetch(BASE_URL)
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=7')
+
+      .then(res => res.json())
+      .then(data => {
+
+        console.log(data);
+
+        data.forEach(todo => {
+            todos.push(todo)
+        });
+
+        listTodos()
+
+    })
+}
+getTodos()
+
+
+
+const listTodos = () => {
+    // todoList.innerHTML = ""
+    todos.forEach(item => {
+
+        const todoElement = createTodoElement(item)
+        todoList.appendChild(todoElement)
+    })
+}
+
+const createTodoElement = (todoData) => {
+    let todo = document.createElement('div')
+    todo.classList.add('item')
+    // console.log(todoData.title);
+    
+    let sak = document.createElement('p')
+    sak.classList.add('sak')
+    
+    sak.innerText = todoData.title
+    todo.appendChild(sak)
+
+
+    return todo
+}
 
 
 
@@ -27,13 +74,17 @@ const createItemElement = (inputValue) => {
   const p = document.createElement('p');
   p.innerText = inputValue;
 
+//   p.addEventListener('click')
+
   const button = document.createElement('button');
   button.innerText = 'Delete';
 
-  // Eventlistener on each and every button////////////////
-  // button.addEventListener('click', () => {
-  //     button.parentElement.remove()
-  // })
+
+
+//   Eventlistener on each and every button////////////////
+//   button.addEventListener('click', () => {
+//       button.parentElement.remove()
+//   })
 
   item.appendChild(p);
   item.appendChild(button);
@@ -41,20 +92,31 @@ const createItemElement = (inputValue) => {
   return item;
 }
 
-
-
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const input = form.querySelector('input[type=text]');
   const inputValue = input.value;
-// Här kan läggas till en popup modal med att fältet är tomt///////////
-  if( inputValue.trim() === '') {
-    return
+  let errorMessage = document.querySelector('#errorMessage');
+
+  //IF satsen ser till att man ej kan skicka in en tom input///////////
+  if (inputValue.trim() === '') {
+    errorMessage.classList.remove('popup_text');
+    return;
+  } else {
+    errorMessage.classList.add('popup_text');
   }
 
-
   const item = createItemElement(inputValue);
-  document.querySelector('#output').appendChild(item)
+  document.querySelector('#output').prepend(item);
+  
 
-  form.reset()
+  form.reset();
 });
+
+// Function to delete todo//////////////
+const deleteTodo = e => {
+    console.log(e.target);
+    console.log(e.target.classList.contains('output'));
+}
+
+output.addEventListener('click', deleteTodo)
